69b60cb05700dacb1487b80590dc3d7ab15458dd
Arunkumar <arunkumar.s@ideas2it.com>
2018-10-11 21:24:44 +0530

    Task: Added Header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,12 +1,59 @@
-import React, {Component} from "react";
-import {Navbar, NavbarBrand} from "reactstrap";
+
+
+
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
 import "./Header";
 
+import logo from '../../../assets/logo.png';
+  
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
-      <div className="ui-header">
-        Pragmatrix
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand>
+            <img src={logo} className="logo-img" />
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink>Devices</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>Discovered Devices</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>Undiscovered Devices</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
     );
   }
