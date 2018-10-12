bef861d5aa4e3e6d097d0ef777ad410db387255c
sachidhanandhan <sachidhanandhan@ideas2it.com>
2018-10-13 03:27:07 +0530

    fix/UI design

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,7 +1,5 @@
 import React, {Component} from "react";
-import { Link } from 'react-router-dom';
 import TaskForm from "./TaskForm";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./Home";
 
 class Home extends Component {
@@ -9,9 +7,6 @@ class Home extends Component {
     return (
       <section className="form-container">
         <header>
-            <Link to="/">
-              <FontAwesomeIcon icon="coffee" />Back
-            </Link>
           <h3>Add Device</h3>
         </header>
         <TaskForm />
